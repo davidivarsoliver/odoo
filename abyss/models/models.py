4d38288f0669bcb93ed5deb1d1f3e8a71e5b99bc
davidivarsoliver <davidivars9@gmail.com>
2023-12-10 19:39:18 +0000

    Projecte 1er Trimestre

--- abyss/models/models.py
+++ abyss/models/models.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+
 from odoo import models, fields, api
 import random, datetime
 from odoo.exceptions import ValidationError
@@ -9,14 +10,58 @@ class Player(models.Model):
     _description = 'abyss.player'
 
     name = fields.Char()
+    level = fields.Integer(default=1)
+    experience = fields.Integer(default=0)
+    money = fields.Float("Money", default=0.0)
     description = fields.Text()
     image = fields.Image(max_width=200)
+    small_image = fields.Image(max_width=50, max_height=50, related='image', store=True)
     age = fields.Integer(readonly=True, compute='_get_age')
     birth_date = fields.Date()
     player_created = fields.Datetime(default=lambda t: fields.Datetime.now())
     success = fields.Boolean(readonly=True)
 
-    abyss_character = fields.Many2many('abyss.character', string="Characters")
+    weapons = fields.Many2many(
+        'abyss.weapon',
+        string="Weapons",
+        widget="many2many_tags",
+        domain="[('required_level', '<=', level)]"
+    )
+    enemies = fields.Many2many('abyss.enemy', string="Enemies")
+    characters = fields.Many2many('abyss.character', string="Characters")
+
+    @api.constrains('experience')
+    def _check_level_up(self):
+        for player in self:
+            if player.experience >= 100 * player.level:
+                player.level += 1
+
+    def gain_experience(self, amount):
+        self.experience += amount
+
+    @api.onchange('weapons')
+    def _onchange_weapons(self):
+        current_cost = sum(weapon.cost for weapon in self.weapons)
+        previous_cost = sum(weapon.cost for weapon in self._origin.weapons)
+
+        refund_amount = previous_cost - current_cost
+        purchase_amount = current_cost - previous_cost
+
+        if purchase_amount > 0:
+            if self.money < purchase_amount:
+                raise ValidationError("No tienes suficiente dinero para comprar estas armas")
+            else:
+                self.money -= purchase_amount
+
+        if refund_amount > 0:
+            self.money += refund_amount
+
+    @api.onchange('characters')
+    def _onchange_characters(self):
+        for character in self.characters:
+            if character.weapon_type and not any(weapon.type == character.weapon_type for weapon in self.weapons):
+                raise ValidationError("No tienes un arma del tipo necesario para este personaje.")
+
 
     @api.depends('birth_date')
     def _get_age(self):
@@ -36,9 +81,89 @@ class Character(models.Model):
 
     name = fields.Char()
     description = fields.Text()
-    type = fields.Selection(
-        [('1', 'Hydro'), ('2', 'Pyro'), ('3', 'Electro'), ('4', 'Geo'), ('5', 'Anemo'), ('6', 'Cryo'), ('7', 'Dendro')])
-    stars = fields.Selection([('1', '4*'), ('2', '5*')])
+    element_type = fields.Selection([
+        ('Hydro', 'Hydro'),
+        ('Pyro', 'Pyro'),
+        ('Electro', 'Electro'),
+        ('Geo', 'Geo'),
+        ('Anemo', 'Anemo'),
+        ('Cryo', 'Cryo'),
+        ('Dendro', 'Dendro')
+    ])
+    weapon_type = fields.Selection([
+        ('Sword', 'Sword'),
+        ('Claymore', 'Claymore'),
+        ('Bow', 'Bow'),
+        ('Catalyst', 'Catalyst'),
+        ('Polearm', 'Polearm')
+    ])
+    stars = fields.Selection([('4', '4*'), ('5', '5*')])
+    health = fields.Integer()
+    is_dead = fields.Boolean(default=False, readonly=True)
+    small_image = fields.Image(max_width=50, max_height=50, related='image', store=True)
     image = fields.Image(max_width=200)
+    level = fields.Integer(default=1)
 
     abyss_players = fields.Many2many('abyss.player', string="Players")
+    enemies = fields.Many2many('abyss.enemy', string="Enemies")
+
+
+    @api.onchange('vida_actual')
+    def _onchange_goal(self):
+            if self.muerto <= 0:
+                self.muerto = 1
+
+    is_immune = fields.Boolean(default=False, compute='_compute_immunity', store=True)
+
+    @api.depends('element_type', 'abyss_players.enemies')
+    def _compute_immunity(self):
+        for character in self:
+            character.is_immune = any(
+                enemy.element_type == character.element_type for player in character.abyss_players for enemy in
+                player.enemies
+            )
+
+
+class Weapon(models.Model):
+    _name = 'abyss.weapon'
+    _description = 'abyss.weapon'
+
+    name = fields.Char()
+    cost = fields.Float(default=0.0)
+    weapon_type = fields.Selection([('Sword', 'Sword'), ('Claymore', 'Claymore'), ('Bow', 'Bow'), ('Catalyst', 'Catalyst'), ('Polearm', 'Polearm')])
+    damage = fields.Integer()
+    small_image = fields.Image(max_width=50, max_height=50, related='image', store=True)
+    required_level = fields.Integer(default=1)
+    image = fields.Image(max_width=200)
+
+
+class Enemy(models.Model):
+    _name = 'abyss.enemy'
+    _description = 'abyss.enemy'
+
+    name = fields.Char()
+    element_type = fields.Selection([
+        ('Hydro', 'Hydro'),
+        ('Pyro', 'Pyro'),
+        ('Electro', 'Electro'),
+        ('Geo', 'Geo'),
+        ('Anemo', 'Anemo'),
+        ('Cryo', 'Cryo'),
+        ('Dendro', 'Dendro')
+    ])
+    health = fields.Integer()
+    damage = fields.Integer()
+    small_image = fields.Image(max_width=50, max_height=50, related='image', store=True)
+    energy = fields.Integer()
+    image = fields.Image(max_width=200)
+
+    @api.constrains('element_type')
+    def _check_valid_element(self):
+        valid_elements = ['Hydro', 'Pyro', 'Electro', 'Geo', 'Anemo', 'Cryo', 'Dendro']
+        if self.element_type not in valid_elements:
+            raise ValidationError("El enemigo es inmune al elemento del personaje")
+
+    @api.constrains('energy')
+    def _check_energy(self):
+        if self.energy <= 0:
+            raise ValidationError("El enemigo no tiene energia")
